c6705a51f46f2de9a9c6a8c03b5823dae03032f9
SantosMaycon <mayconalves.santos14@gmail.com>
2020-03-30 00:48:20 -0300

    Inserção de usuário com POST

--- src/main/java/com/maycon/workshopmongo/services/UserService.java
+++ src/main/java/com/maycon/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.maycon.workshopmongo.domain.User;
+import com.maycon.workshopmongo.dto.UserDTO;
 import com.maycon.workshopmongo.repository.UserRepository;
 import com.maycon.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO obj) {
+		return new User(obj.getId(), obj.getName(), obj.getEmail());
 	}
 }

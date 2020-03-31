5f62bcb6b1e7d158b989bf9e0e334ea70e8e7fa2
SantosMaycon <mayconalves.santos14@gmail.com>
2020-03-31 01:47:18 -0300

    Consulta simples com query methods

--- src/main/java/com/maycon/workshopmongo/resources/PostResource.java
+++ src/main/java/com/maycon/workshopmongo/resources/PostResource.java
@@ -7,9 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.maycon.workshopmongo.domain.Post;
+import com.maycon.workshopmongo.resources.util.URL;
 import com.maycon.workshopmongo.services.PostService;
 
 @RestController
@@ -30,4 +32,12 @@ public class PostResource {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+	
 }

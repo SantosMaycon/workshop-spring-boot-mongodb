0c1ea3da1ed42f71c0bbc5c5713a907b0f54e41a
SantosMaycon <mayconalves.santos14@gmail.com>
2020-03-30 22:20:27 -0300

    Obtendo um post por id

---
+++ src/main/java/com/maycon/workshopmongo/resources/PostResource.java
@@ -0,0 +1,33 @@
+package com.maycon.workshopmongo.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.maycon.workshopmongo.domain.Post;
+import com.maycon.workshopmongo.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+	@Autowired
+	private PostService service;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findAll(){
+		List<Post> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/{id}")
+	public ResponseEntity<Post> findById(@PathVariable("id") String id){
+		Post obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
